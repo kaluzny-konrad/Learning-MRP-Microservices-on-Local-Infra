8d99df308916bf551d44c5b4e27b22d3e75ac158
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-07-20 20:28:12 +0200

    add cat service

--- Database/.eslintrc.js
+++ Database/.eslintrc.js
@@ -5,10 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   root: true,
   env: {
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
---
+++ Database/Readme.md
@@ -0,0 +1,10 @@
+# Database Backend API with Nest, Prisma and SQLite
+
+## Info about Nest
+
+- To create a controller using the CLI, simply execute the `nest g controller [name]` command.
+
+
+## Eslint
+
+- if problem, run in command pallette: `ESLint: Restart ESLint server`
